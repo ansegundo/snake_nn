12adc54e5a4611d6f2d58dfa3987137f42a519d9
ansegundo <ansegundo@hotmail.com>
2018-05-20 23:44:34 +0000

    setting up project

---
+++ snake.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
+var snake;
+var snake_size = 5;
+var wid = 400;
+var height = 400;
+var score = 0;
+var fruit;
+
