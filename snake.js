cb716b33afa6ad55262a425d90e7dc195aca3e9d
ansegundo <ansegundo@hotmail.com>
2018-05-21 23:41:55 +0000

    distance calc

--- snake.js
+++ snake.js
@@ -1,98 +1,41 @@
 var drawInit = ( function() {
 
+
+    // coloring the snake's and fruit's body
     var snakeBody = function(x, y) {
-        context.fillStyle = 'green';
+        context.fillStyle = 'black';
         context.fillRect(x*snake_size, y*snake_size, snake_size, snake_size);
-        context.strokeStyle = 'darkgreen';
+        context.strokeStyle = 'white';
         context.strokeRect(x*snake_size, y*snake_size, snake_size, snake_size);
     }
 
     var pizza = function(x, y) {
-        context.fillStyle = 'yellow';
+        context.fillStyle = 'blue';
         context.fillRect(x*snake_size, y*snake_size, snake_size, snake_size);
-        context.fillStyle = 'red';
+        context.fillStyle = 'yellow';
         context.fillRect(x*snake_size+1, y*snake_size+1, snake_size-2, snake_size-2);
     }
 
+    // text on screen
     var scoreText = function() {
         var score_text = "Score: " + score;
-        context.fillStyle = 'blue';
+        context.fillStyle = 'red';
         context.fillText(score_text, 145, hei-5);
     }
 
     var epochText = function () {
         var epoch_text = "Epoch: " + epoch;
-        context.fillStyle = 'blue';
+        context.fillStyle = 'red';
         context.fillText(epoch_text, 245, hei-5);
     }
 
+    //setting up snake parts
     var drawSnake = function() {
-        var length = 4;
+        var length = 3;
         snake = [];
         for (var i = length-1; i>=0; i--) {
             snake.push({x:i, y:0});
         }  
-    }
-
-    var paint = function() {
-        context.fillStyle = 'lightgrey';
-        context.fillRect(0, 0, wid, hei);
-        context.strokeStyle = 'black';
-        context.strokeRect(0, 0, wid, hei);
-
-        btn_start.setAttribute('disabled', true);
-
-        var snakeX = snake[0].x;
-        var snakeY = snake[0].y;
-
-        if (direction == 'right') { 
-            snakeX++; 
-        }
-          else if (direction == 'left') { 
-            snakeX--; 
-        }
-          else if (direction == 'up') { 
-            snakeY--; 
-        } else if(direction == 'down') { 
-            snakeY++; 
-        }
-
-        if (snakeX == -1 || snakeX == wid / snake_size || snakeY == -1 || snakeY == hei / snake_size || checkCollision(snakeX, snakeY, snake)) {
-             
-            btn_start.removeAttribute('disabled', true);
-
-            context.clearRect(0, 0, wid, hei);
-            gameloop = clearInterval(gameloop);
-            return;
-
-        }
-
-        if (snakeX == food.x && snakeY == food.y){
-            var tail = {
-                x: snakeX,
-                y: snakeY
-            };
-            score++;
-
-            createFood();
-        }
-        else {
-            var tail = snake.pop();
-            tail.x = snakeX;
-            tail.y = snakeY;
-        }
-
-        snake.unshift(tail);
-
-        for (var i = 0; i < snake.length; i++) {
-            snakeBody(snake[i].x, snake[i].y);
-        }
-
-        pizza(food.x, food.y);
-
-        scoreText();
-        epochText();
-        epoch++;
     }
 
     var createFood = function() {
@@ -120,11 +63,94 @@ var drawInit = ( function() {
         return false;
     }
 
+    var distance = function(food, snakeX, snakeY) {
+        var a = snakeX - food.x;
+        var b = snakeY - food.y;
+        var c = Math.sqrt( a*a + b*b );
+        console.log(c)
+    }
+
+
+    var paint = function() {
+
+        // coloring the board
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, wid, hei);
+        context.strokeStyle = 'black';
+        context.strokeRect(0, 0, wid, hei);
+
+        btn_start.setAttribute('disabled', true);
+
+        var snakeX = snake[0].x; 
+        var snakeY = snake[0].y;
+        // console.log('snake position: ' + snake[0].x + ',' + snake[0].y)
+
+        if (direction == 'right') { 
+            snakeX++; 
+        }
+        else if (direction == 'left') { 
+            snakeX--; 
+        }
+        else if (direction == 'up') { 
+            snakeY--; 
+        } 
+        else if(direction == 'down') { 
+            snakeY++; 
+        }
+
+        if (snakeX == -1 || 
+            snakeX == wid / snake_size || 
+            snakeY == -1 || 
+            snakeY == hei / snake_size || 
+            checkCollision(snakeX, snakeY, snake)) {
+             
+            btn_start.removeAttribute('disabled', true);
+
+            context.clearRect(0, 0, wid, hei);
+            gameloop = clearInterval(gameloop);
+            return;
+        }
+
+        if (snakeX == food.x && snakeY == food.y){
+            var tail = {
+                x: snakeX,
+                y: snakeY
+            };
+            score++;
+            createFood();
+        }
+        else {
+            var tail = snake.pop();
+            tail.x = snakeX;
+            tail.y = snakeY;
+        }
+
+        snake.unshift(tail);
+
+        for (var i = 0; i < snake.length; i++) {
+            snakeBody(snake[i].x, snake[i].y);
+        }
+
+        pizza(food.x, food.y);
+        scoreText();
+        epochText();
+        distance(food, snakeX, snakeY);
+        epoch++;
+
+        /*
+        test = Math.floor(Math.random() * 4) + 1;
+        console.log(test);
+        if (test == 1){
+            direction = 'right';
+        }
+        */
+    }
+
     var init = function(){
-        direction = 'down';
+        direction = 'right';
         drawSnake();
         createFood();
-        gameloop = setInterval(paint, 80);
+        gameloop = setInterval(paint, 100);
     }
 
     return {
